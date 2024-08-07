aa9f21270d7bdfe45ec10dc2d748e6a564737d76
adeerioo <ade.120230048@student.itera.ac.id>
2024-08-07 15:17:11 +0700

    Belajar project baru

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("Biodata")
 
+// Exercise Variable
 var name = "Ade Rio Saputra";
 console.log(name);
 
@@ -12,3 +13,58 @@ console.log(university);
 var citizen = "Lampung"
 console.log(citizen);
 
+//Exercise Function
+function Animal (name, type = "Mamalia") {
+    // penggunaan string literal atau ' ketika ingin menggunakan variabel/parameter harus dengan${}
+   return `${name} merupakan binatang dari jenis ${type}`;
+}
+
+const greeting = Animal("Paus", "Mamalia"); // => jika function memiliki keywords return didalamnya bisa menggunakan variable
+
+console.log(greeting);
+
+// Exercise Loop
+const teman = ["Ghaza", "Reyhan", "Fatur","Afif","Lia"]
+for (let i =0; i < teman.length; i++) {
+    console.log(teman[i]);
+}
+
+console.log("Perulangan Berhasil");
+
+// Exercise Class
+class Mahasiswa {
+    constructor(name, dapartement) {
+      this._name = name;
+      this._dapartement = dapartement;
+    }
+
+    information() {
+        return `Hallo, nama saya ${this.name} dan saya dari jurusan ${this._dapartement}.`;
+    }
+    //Ini adalah setter untuk merubah nilai dari sebuah property
+    setName(name) {
+        this._name = name;
+    }
+
+    //Ini adalah setter untuk mengambil data dari sebuah
+    getName() {
+        return this.name;
+    }
+
+    //Ini merupakan contoh penggunaan setter dengan keyword set
+    set name(value) {
+        if (value !== "") {
+            this._name = value;
+        } else {
+            console.log("The value must be not  empty string.");
+        }
+    }
+
+    //Ini merupakan contoh penggunaan getter dengan keyword get
+    get name() {
+        return this._name;
+    }
+}
+
+const mahasiswa = new Mahasiswa("Ade Rio Saputra", "Teknik Geomatika");
+console.log(mahasiswa.information());
