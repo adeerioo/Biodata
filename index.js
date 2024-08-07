d88e7f3fc61381539397a685a5fe9fe518f53c17
adeerioo <ade.120230048@student.itera.ac.id>
2024-08-07 14:22:42 +0700

    Belajar project baru

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Biodata")
+
+var name = "Ade Rio Saputra";
+console.log(name);
+
+var age = 22
+console.log(age);
+
+var university = "Institut Teknologi Sumatera"
+console.log(university);
+
+var citizen = "Lampung"
+console.log(citizen);
+
